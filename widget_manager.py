fc0d9948dec9422d788cfe997842b401a58043b3
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-03-13 14:53:58 +0000

    developed better system for creating and changing the image display, loading image now working

--- widget_manager.py
+++ widget_manager.py
@@ -4,38 +4,43 @@ import os
 
 
 class WidgetManager(Frame):
-    def __init__(self, master=None, upload_logo='upload_logo.png'):
+    def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
-        self.upload_logo = PhotoImage(file=os.path.join(os.getcwd(), upload_logo))
+        # define default widget images
+        self.upload_logo = PhotoImage(file=os.path.join(os.getcwd(), 'upload_logo.png'))
         self.upload_logo = self.upload_logo.subsample(3, 3)
-        self.error_msg = Label(self.master, text="", fg="red")
+        self.output_logo = PhotoImage(file=os.path.join(os.getcwd(), 'eye_logo.png'))
+        self.output_logo = self.output_logo.subsample(3, 3)
+        # initialise label members
+        self.error_msg = Label(self.master, text="", fg="red", font=("Helvetica", 20))
         self.error_msg.pack(side=BOTTOM)
         self.output_img = None
-        self.source_img = None
+        self.input_img = None
+        # generate initial widget layout
         self.create_widget_layout()
 
     def create_widget_layout(self):
         # generate canvas to hold images
-        upload_canvas = self.create_placeholder_canvas(E)
-        #output_canvas = self.create_placeholder_canvas(W)
-        # create upload image stock
-        self.source_img = Label(upload_canvas, image=self.upload_logo)
-        self.source_img.configure(image=self.upload_logo)
-        self.source_img.place(relx=0.5, rely=0.5, anchor=CENTER)
+        upload_canvas, self.input_img = self.create_placeholder_canvas(E, self.upload_logo)
+        _, self.output_img = self.create_placeholder_canvas(W, self.output_logo)
         # add button for configuring images
         choose_file = Button(upload_canvas,
                              text='Choose File',
                              command=lambda: self.load_img())
         choose_file.place(relx=0.5, rely=0.9, anchor=N)
 
-    def create_placeholder_canvas(self, anchor):
+    def create_placeholder_canvas(self, anchor, default_image):
         # generate canvas to hold images
-        canvas = Canvas(self.master, bg='#eaeaea')
+        canvas = Canvas(self.master)
         # expand and center canvas
         canvas.pack(expand=0)
         canvas.place(relx=0.5, rely=0.5, anchor=anchor)
-        return canvas
+        # configure placeholder image
+        label_placeholder = Label(canvas, image=default_image)
+        label_placeholder.configure(image=default_image)
+        label_placeholder.place(relx=0.5, rely=0.5, anchor=CENTER)
+        return canvas, label_placeholder
 
     def load_img(self):
         img_path = askopenfilename(initialdir=os.getcwd(),
@@ -47,7 +52,7 @@ class WidgetManager(Frame):
         if os.path.isfile(img_path):
             try:
                 self.upload_logo = PhotoImage(file=img_path)
-                self.source_img.configure(image=self.upload_logo)
+                self.input_img.configure(image=self.upload_logo)
             except:
                 self.error_msg.configure(text="Failed to load File!")
 
